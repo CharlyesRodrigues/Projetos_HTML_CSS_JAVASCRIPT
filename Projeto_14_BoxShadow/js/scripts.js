c4f1608b7da1ccfca961fa0026edeee14594fa44
Charlyes <charlyessouza@gmail.com>
2024-07-03 12:58:34 -0300

    Criando e finalizando eventos do box Shadow

--- Projeto_14_BoxShadow/js/scripts.js
+++ Projeto_14_BoxShadow/js/scripts.js
@@ -5,8 +5,8 @@ constructor(
     horizontalRef, 
     vertical, 
     verticalRef,
-    blue,
-    blueRef,
+    blur,
+    blurRef,
     spread,
     spreadRef,
     previewBox,
@@ -19,8 +19,8 @@ constructor(
     this.horizontalRef = horizontalRef;
     this.vertical      = vertical;
     this.verticalRef   = verticalRef;
-    this.blue          = blue;
-    this.blueRef       = blueRef;
+    this.blur          = blur;
+    this.blurRef       = blurRef;
     this.spread        = spread;
     this.spreadRef     = spreadRef;
     this.previewBox    = previewBox;
@@ -30,6 +30,55 @@ constructor(
 
   }
 
+  inicialize() {
+
+    this.horizontalRef.value = this.horizontal.value;
+    this.verticalRef.value = this.vertical.value;
+    this.blurRef.value = this.blur.value;
+    this.spreadRef.value = this.spread.value
+
+    this.applyRule()
+    this.showRule()
+  }
+
+  applyRule(){
+
+    this.previewBox.style.boxShadow = `
+    ${this.horizontalRef.value}px
+    ${this.verticalRef.value}px
+    ${this.blurRef.value}px
+    ${this.spreadRef.value}px  #000000
+    `
+    this.currentRule = this.previewBox.style.boxShadow
+  }
+  showRule(){
+
+    this.rule.innerText = this.currentRule
+    this.webkitRule.innerText = this.currentRule
+    this.mozRule.innerText = this.currentRule
+
+  }
+
+  updateValue(type, value){
+
+    switch (type){
+      case "horizontal":
+        this.horizontalRef.value = value;
+        break;
+      case "vertical":
+        this.verticalRef.value = value
+        break;
+      case "spread":
+        this.spreadRef.value = value
+        break;
+      case "blur":
+        this.blurRef.value = value
+        break;
+
+    }
+    this.applyRule()
+    this.showRule()
+  }
 }
 
 /*Selecionar elementos */
@@ -38,13 +87,12 @@ const horizontal = document.querySelector("#horizontal")
 const horizontalRef = document.querySelector("#horizontal-value")
 const vertical = document.querySelector("#vertical")
 const verticalRef = document.querySelector("#vertical-value")
-const blue = document.querySelector("blue")
-const blueRef = document.querySelector("#blue-value")
+const blur = document.querySelector("#blur")
+const blurRef = document.querySelector("#blur-value")
 const spread = document.querySelector("#spread")
 const spreadRef = document.querySelector("#spread-value")
 
 const previewBox =document.querySelector("#box")
-
 const rule =document.querySelector("#rule span")
 const webkitRule =document.querySelector("#webkit-rule span")
 const mozRule =document.querySelector("#moz-rule span")
@@ -57,8 +105,8 @@ const boxShadow = new boxShadowGenerator(
     horizontalRef, 
     vertical, 
     verticalRef,
-    blue,
-    blueRef,
+    blur,
+    blurRef,
     spread,
     spreadRef,
     previewBox,
@@ -67,5 +115,33 @@ const boxShadow = new boxShadowGenerator(
     mozRule
 )
 
-console.log(boxShadow)
+boxShadow.inicialize();
 /*Eventos */
+
+horizontal.addEventListener("input" , (e)=>{
+
+const value = e.target.value;
+
+boxShadow.updateValue("horizontal", value)
+})
+
+vertical.addEventListener("input" , (e)=>{
+
+  const value = e.target.value;
+  
+  boxShadow.updateValue("vertical", value)
+  })
+
+  spread.addEventListener("input" , (e)=>{
+
+    const value = e.target.value;
+    
+    boxShadow.updateValue("spread", value)
+    })
+
+    blur.addEventListener("input" , (e)=>{
+
+      const value = e.target.value;
+      
+      boxShadow.updateValue("blur", value)
+      })
