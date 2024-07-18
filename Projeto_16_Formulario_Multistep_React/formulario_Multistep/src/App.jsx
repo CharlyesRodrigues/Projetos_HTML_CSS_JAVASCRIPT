661a8619c0beeb711fa1f3ce101e2f3bd7742429
Charlyes <charlyessouza@gmail.com>
2024-07-18 20:04:31 -0300

    Criando o Hook userForm

--- Projeto_16_Formulario_Multistep_React/formulario_Multistep/src/App.jsx
+++ Projeto_16_Formulario_Multistep_React/formulario_Multistep/src/App.jsx
@@ -1,10 +1,24 @@
 // Components
 
 import { GrFormNext, GrFormPrevious } from "react-icons/gr"
+
+import UserForm from "./components/UserForm"
+import ReviewForm from "./components/ReviewForm"
+import Thanks from "./components/Thanks"
+
+// Hooks
+import { useForm } from "./hooks/useForm"
+
+// Estilos
 import './App.css'
+
 
 function App() {
 
+ 
+  const formComponents = [<UserForm />, <ReviewForm />, <Thanks />]
+
+  const {currentStep,currentComponent} = useForm (formComponents)
 
   return (
     <div className='app'>
@@ -16,9 +30,7 @@ function App() {
       <div className="form-container">
         <p>etapas</p>
         <form >
-          <div className="inputs-container">
-
-          </div>
+          <div className="inputs-container">{currentComponent}</div>
           <div className="actions">
             <button type='button'>
               <GrFormPrevious />
---
+++ Projeto_16_Formulario_Multistep_React/formulario_Multistep/src/hooks/useForm.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export function useForm (steps){
+    const [currentStep, setCurrentStep] = useState(0)
+
+    return {
+        currentStep,
+        currentComponent : steps[currentStep]
+
+
+    }
+
+}
