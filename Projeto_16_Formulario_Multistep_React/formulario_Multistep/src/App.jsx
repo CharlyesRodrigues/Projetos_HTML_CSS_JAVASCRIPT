1f884d32162d8838a8f306863c7bf18ee3f32a20
Charlyes <charlyessouza@gmail.com>
2024-07-18 19:11:12 -0300

    Adicionando fontes, Ícones e Criando a base do projeto

--- Projeto_16_Formulario_Multistep_React/formulario_Multistep/src/App.jsx
+++ Projeto_16_Formulario_Multistep_React/formulario_Multistep/src/App.jsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// Components
+
+import { GrFormNext, GrFormPrevious } from "react-icons/gr"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='app'>
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>Ficamos felizes com sua compra, utilize o formulário abaixo para avaliar o produto</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="form-container">
+        <p>etapas</p>
+        <form >
+          <div className="inputs-container">
+
+          </div>
+          <div className="actions">
+            <button type='button'>
+              <GrFormPrevious />
+              <span>Voltar</span>
+            </button>
+            <button type='submit'>
+              <span>Avançar</span>
+            <GrFormNext />
+            </button>
+          </div>
+
+        </form>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
