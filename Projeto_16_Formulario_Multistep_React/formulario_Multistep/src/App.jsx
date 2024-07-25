f20f269719632e2dc5f23e480b1fd35fb6d7e7e8
Charlyes <charlyessouza@gmail.com>
2024-07-25 14:58:26 -0300

    Criando a funcionalidade de mudar de etapa

--- Projeto_16_Formulario_Multistep_React/formulario_Multistep/src/App.jsx
+++ Projeto_16_Formulario_Multistep_React/formulario_Multistep/src/App.jsx
@@ -1,7 +1,7 @@
 // Components
 
 import { GrFormNext, GrFormPrevious } from "react-icons/gr"
-
+import {FiSend} from "react-icons/fi"
 import UserForm from "./components/UserForm"
 import ReviewForm from "./components/ReviewForm"
 import Thanks from "./components/Thanks"
@@ -15,10 +15,11 @@ import './App.css'
 
 function App() {
 
- 
+
   const formComponents = [<UserForm />, <ReviewForm />, <Thanks />]
 
-  const {currentStep,currentComponent} = useForm (formComponents)
+  // O useState abaixo esta sendo desestruturado do hooks que criei
+  const { currentStep, currentComponent, changeStep, isLastStep, isFirstStep } = useForm(formComponents)
 
   return (
     <div className='app'>
@@ -29,17 +30,26 @@ function App() {
 
       <div className="form-container">
         <p>etapas</p>
-        <form >
+        <form onSubmit={(e) => changeStep(currentStep + 1, e)} >
           <div className="inputs-container">{currentComponent}</div>
           <div className="actions">
-            <button type='button'>
+          {!isFirstStep && (
+              <button type='button' onClick={() => changeStep(currentStep - 1)} >
               <GrFormPrevious />
               <span>Voltar</span>
             </button>
-            <button type='submit'>
+          )}
+            {!isLastStep ? (
+              <button type='submit'>
               <span>Avançar</span>
-            <GrFormNext />
+              <GrFormNext />
             </button>
+            ):(
+              <button type='button'>
+              <span>Enviar</span>
+             <FiSend />
+            </button>
+            )}
           </div>
 
         </form>
