b1a5d1a468c8ea8df8f5437d8aaf43c09770a24b
Charlyes <charlyessouza@gmail.com>
2024-07-26 13:56:15 -0300

    Update

--- Projeto_16_Formulario_Multistep_React/formulario_Multistep/src/App.jsx
+++ Projeto_16_Formulario_Multistep_React/formulario_Multistep/src/App.jsx
@@ -1,64 +1,79 @@
 // Components
+import UserForm from "./components/UserForm";
+import ReviewForm from "./components/ReviewForm";
+import Thanks from "./components/Thanks";
+import Steps from "./components/Steps";
 
-import { GrFormNext, GrFormPrevious } from "react-icons/gr"
-import {FiSend} from "react-icons/fi"
-import UserForm from "./components/UserForm"
-import ReviewForm from "./components/ReviewForm"
-import Thanks from "./components/Thanks"
-import Steps from "./components/Steps"
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+import { FiSend } from "react-icons/fi";
 
 // Hooks
-import { useForm } from "./hooks/useForm"
+import { useState } from "react";
+import { useForm } from "./hooks/useForm";
 
-// Estilos
-import './App.css'
+// CSS
+import "./App.css";
 
-
+const formTemplate = {
+  name: "",
+  email: "",
+  review: "",
+  comment: "",
+};
 
 function App() {
+  const [data, setData] = useState(formTemplate);
 
+  const updateFieldHandler = (key, value) => {
+    setData((prev) => {
+      return { ...prev, [key]: value };
+    });
+  };
 
-  const formComponents = [<UserForm />, <ReviewForm />, <Thanks />]
+  const formComponents = [
+    <UserForm data={data} updateFieldHandler={updateFieldHandler} />,
+    <ReviewForm data={data} updateFieldHandler={updateFieldHandler} />,
+    <Thanks data={data} />,
+  ];
 
-  // O useState abaixo esta sendo desestruturado do hooks que criei
-  const { currentStep, currentComponent, changeStep, isLastStep, isFirstStep } = useForm(formComponents)
+  const { currentStep, currentComponent, changeStep, isLastStep } =
+    useForm(formComponents);
 
   return (
-    <div className='app'>
+    <div className="app">
       <div className="header">
         <h2>Deixe sua avaliação</h2>
-        <p>Ficamos felizes com sua compra, utilize o formulário abaixo para avaliar o produto</p>
+        <p>
+          Ficamos felizes com a sua compra, utilize o formulário abaixo para
+          avaliar o produto
+        </p>
       </div>
-
       <div className="form-container">
-        <Steps  currentStep={currentStep}/>
-    
-        <form onSubmit={(e) => changeStep(currentStep + 1, e)} >
+        <Steps currentStep={currentStep} />
+        <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
           <div className="inputs-container">{currentComponent}</div>
           <div className="actions">
-          {!isFirstStep && (
-              <button type='button' onClick={() => changeStep(currentStep - 1)} >
+            <button type="button" onClick={() => changeStep(currentStep - 1)}>
               <GrFormPrevious />
               <span>Voltar</span>
             </button>
-          )}
+
             {!isLastStep ? (
-              <button type='submit'>
-              <span>Avançar</span>
-              <GrFormNext />
-            </button>
-            ):(
-              <button type='button'>
-              <span>Enviar</span>
-             <FiSend />
-            </button>
+              <button type="submit">
+                <span>Avançar</span>
+                <GrFormNext />
+              </button>
+            ) : (
+              <button type="button">
+                <span>Enviar</span>
+                <FiSend />
+              </button>
             )}
           </div>
-
         </form>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- Projeto_16_Formulario_Multistep_React/formulario_Multistep/src/hooks/useForm.jsx
+++ Projeto_16_Formulario_Multistep_React/formulario_Multistep/src/hooks/useForm.jsx
@@ -1,23 +1,20 @@
 import { useState } from "react";
 
-
 export function useForm(steps) {
-    const [currentStep,setCurrentStep] = useState(0)
-    function changeStep(i, e) {
-        
-        if (e) e.preventDefault();
+  const [currentStep, setCurrentStep] = useState(0);
 
-        if ( i < 0 || i >= steps.length) return
+  function changeStep(i, e) {
+    if (e) e.preventDefault();
 
-        setCurrentStep(i)
+    if (i < 0 || i >= steps.length) return;
 
-    }
-    return {
-        currentStep,
-        currentComponent: steps[currentStep],
-        changeStep,
-        isLastStep: currentStep + 1 === steps.length ? true:false,
-        isFirstStep : currentStep === 0 ? true:false ,
-    }
+    setCurrentStep(i);
+  }
 
+  return {
+    currentStep,
+    isLastStep: currentStep + 1 === steps.length ? true : false,
+    currentComponent: steps[currentStep],
+    changeStep,
+  };
 }
