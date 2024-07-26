4adcaa20b03321cac45bdae2c8a4de53e908fcfd
Charlyes <charlyessouza@gmail.com>
2024-07-26 12:17:39 -0300

    Criando e estilizando componente de exibição de  estágio

--- Projeto_16_Formulario_Multistep_React/formulario_Multistep/src/App.jsx
+++ Projeto_16_Formulario_Multistep_React/formulario_Multistep/src/App.jsx
@@ -5,12 +5,14 @@ import {FiSend} from "react-icons/fi"
 import UserForm from "./components/UserForm"
 import ReviewForm from "./components/ReviewForm"
 import Thanks from "./components/Thanks"
+import Steps from "./components/Steps"
 
 // Hooks
 import { useForm } from "./hooks/useForm"
 
 // Estilos
 import './App.css'
+
 
 
 function App() {
@@ -29,6 +31,7 @@ function App() {
       </div>
 
       <div className="form-container">
+        <Steps  currentStep={currentStep}/>
     
         <form onSubmit={(e) => changeStep(currentStep + 1, e)} >
           <div className="inputs-container">{currentComponent}</div>
